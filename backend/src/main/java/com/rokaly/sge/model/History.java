b37e515d8d2792647f06429ab248d610610e489f
Vitor Mateus <vmateusaguiar@gmail.com>
2025-10-06 22:49:35 -0300

    feat: History create method

--- backend/src/main/java/com/rokaly/sge/model/History.java
+++ backend/src/main/java/com/rokaly/sge/model/History.java
@@ -1,5 +1,6 @@
 package com.rokaly.sge.model;
 
+import com.rokaly.sge.dto.HistoryDTO;
 import jakarta.persistence.*;
 
 @Entity
@@ -12,4 +13,51 @@ public class History {
     private String dateTime;
     @ManyToOne
     private Forklift forklift;
+
+    public History(HistoryDTO data, Forklift forklift) {
+        this.hourMeter = data.hourMeter();
+        this.description = data.description();
+        this.dateTime = data.dateTime();
+        this.forklift = forklift;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Double getHourMeter() {
+        return hourMeter;
+    }
+
+    public void setHourMeter(Double hourMeter) {
+        this.hourMeter = hourMeter;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Forklift getForklift() {
+        return forklift;
+    }
+
+    public void setForklift(Forklift forklift) {
+        this.forklift = forklift;
+    }
 }
